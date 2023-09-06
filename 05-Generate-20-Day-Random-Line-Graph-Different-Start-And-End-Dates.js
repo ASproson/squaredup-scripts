5a1a8a8ae7cbcbdca21ffc05de7958595a87c447
Atlas Sproson <a.sproson@outlook.com>
2023-09-06 13:17:56 +0100

    Create 05-Generate-20-Day-Random-Line-Graph-Different-Start-And-End-Dates.js

---
+++ 05-Generate-20-Day-Random-Line-Graph-Different-Start-And-End-Dates.js
@@ -0,0 +1,34 @@
+async function getData(params, api) {
+    const demoData = [];
+
+    function generateData(serviceName) {
+        const startDate = new Date('2023-01-01T00:00:00.000Z');
+        let currentDate = new Date(startDate);
+
+        const random = Math.random() * 40 - 20
+
+        for (let i = 0; i < 20; i++) {
+            demoData.push({
+                service: serviceName,
+                currency: 'usd', 
+                date: currentDate.toISOString(),
+                cost: Math.random(100)
+            });
+
+            currentDate.setDate(currentDate.getDate() + 1); 
+        }
+    }
+
+    generateData('DataOne');
+    generateData('DataTwo');
+
+    demoData.shift()
+    demoData.shift()
+    demoData.shift()
+    demoData.shift()
+
+    demoData.pop()
+    demoData.pop()
+    
+    return demoData
+}
